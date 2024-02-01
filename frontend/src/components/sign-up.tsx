d7359fe1821ad9babf0fca96ef1b48230cfea30c
paulkreft <paulykreft@gmail.com>
2024-02-01 21:48:36 +0100

    add header layout; add login and signup page

---
+++ frontend/src/components/sign-up.tsx
@@ -0,0 +1,14 @@
+export default function SignUp() {
+  const link = import.meta.env.PROD
+    ? "/oauth2/authorization/github"
+    : "http://localhost:8080/oauth2/authorization/github";
+
+  return (
+    <div className="mx-auto flex flex-1 items-center justify-center pb-20">
+      <button className="flex h-max items-center rounded-lg border border-black px-5 py-4 text-xl font-light">
+        <img className="mr-3 h-8" src="/src/assets/github-mark.png" alt="GitHub" />
+        <a href={link}>Sign up with GitHub</a>
+      </button>
+    </div>
+  );
+}
