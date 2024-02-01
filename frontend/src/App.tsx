c528b75e5e4981bb50ff160b92ef699aa47b85b8
paulkreft <paulykreft@gmail.com>
2024-02-01 08:44:36 +0100

    format

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,7 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-    return (
-        <>
-            Welcome
-        </>
-    )
+  return <>Welcome</>;
 }
 
-export default App
+export default App;
