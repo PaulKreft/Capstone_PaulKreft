8a4fe0c8101d00a33510a6d01a92d614ea661f12
paulkreft <paulykreft@gmail.com>
2024-02-01 16:39:26 +0100

    add basic play page with route and button to navigate to from home

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Play from "./components/play.tsx";
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/home.tsx";
 import NotFound from "./components/not-found.tsx";
@@ -9,6 +10,7 @@ function App() {
       <div className="flex min-h-screen flex-col">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/play" element={<Play />} />
           <Route path={"/*"} element={<NotFound />} />
         </Routes>
       </div>
