2145ec11e4dd84e311e7dd6f7aa7455911fa26c9
paulkreft <paulykreft@gmail.com>
2024-02-01 16:27:49 +0100

    add react router with basic Home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,17 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/home.tsx";
 
 function App() {
-  return <>Welcome</>;
+  return (
+    <>
+      <div className="flex min-h-screen flex-col">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/home.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div className="flex flex-1 flex-col items-center justify-center pb-52">
+      <h1 className="text-center text-8xl font-extrabold leading-snug">
+        Welcome to <span className="text-[#3A0842]">H</span>
+        <span className="text-[#9AB87A]">e</span>
+        <span className="text-[#444B6E]">x</span>
+        <span className="text-[#247BA0]">H</span>
+        <span className="text-[#DC851F]">e</span>
+        <span className="text-[#F8F991]">x</span>
+        <span className="text-[#720026]">!</span>
+      </h1>
+    </div>
+  );
+}
