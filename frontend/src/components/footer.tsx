c3f7bfa356f157b67ac25f7f3a2941e551e43564
paulkreft <paulykreft@gmail.com>
2024-02-02 15:03:46 +0100

    make footer less apparent

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   const currentYear: number = new Date().getFullYear();
 
   return (
-    <div className="flex h-20 w-full flex-col items-center justify-center gap-4 bg-gray-50 px-5 text-xs text-gray-400/75 sm:h-10 sm:flex-row sm:justify-normal">
+    <div className="flex h-10 w-full flex-col items-center justify-end gap-1 bg-gray-50 px-5 text-xs text-gray-400/75 sm:flex-row sm:justify-normal sm:gap-4">
       © {currentYear} HexHex Game. All rights reserved.
       <div className="flex gap-4">
         <Link to="/">Terms & Conditions</Link>
