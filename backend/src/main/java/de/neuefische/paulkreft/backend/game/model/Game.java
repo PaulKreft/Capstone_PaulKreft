614b1781baaf6816c859e8b08681e0549498e963
paulkreft <paulykreft@gmail.com>
2024-02-06 21:14:20 +0100

    add game and tile class

---
+++ backend/src/main/java/de/neuefische/paulkreft/backend/game/model/Game.java
@@ -0,0 +1,19 @@
+package de.neuefische.paulkreft.backend.game.model;
+
+import org.springframework.data.annotation.Id;
+
+import java.time.Instant;
+import java.util.List;
+
+public record Game(
+        @Id
+        String id,
+        String userId,
+        String type,
+        int difficulty,
+        boolean isSuccess,
+        int duration,
+        List<Tile> configuration,
+        Instant createdAt
+) {
+}
