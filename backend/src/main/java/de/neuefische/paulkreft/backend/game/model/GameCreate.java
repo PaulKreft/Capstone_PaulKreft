d83b9e4500c89227b40b33680a6e2acbfbb12bc3
paulkreft <paulykreft@gmail.com>
2024-02-06 21:15:07 +0100

    create GameCreate record

---
+++ backend/src/main/java/de/neuefische/paulkreft/backend/game/model/GameCreate.java
@@ -0,0 +1,18 @@
+package de.neuefische.paulkreft.backend.game.model;
+
+import java.time.Instant;
+import java.util.List;
+
+public record GameCreate(
+        String userId,
+        String type,
+        int difficulty,
+        boolean isSuccess,
+        int duration,
+        List<Tile> configuration
+) {
+
+    public Game withIdAndCreatedAt(String id, Instant createdAt) {
+        return new Game(id, this.userId, this.type, this.difficulty, this.isSuccess, this.duration, this.configuration, createdAt);
+    }
+}
