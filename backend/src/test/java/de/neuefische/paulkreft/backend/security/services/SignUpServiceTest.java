88b58e82f3927b77770cbdeda48c26e6b53e558f
paulkreft <paulykreft@gmail.com>
2024-02-07 16:30:32 +0100

    fix tests to except exception instead of null

--- backend/src/test/java/de/neuefische/paulkreft/backend/security/services/SignUpServiceTest.java
+++ backend/src/test/java/de/neuefische/paulkreft/backend/security/services/SignUpServiceTest.java
@@ -1,18 +1,19 @@
 package de.neuefische.paulkreft.backend.security.services;
 
+import de.neuefische.paulkreft.backend.exception.EmailAlreadyRegisteredException;
 import de.neuefische.paulkreft.backend.security.models.SignUpRequest;
 import de.neuefische.paulkreft.backend.users.models.UserGet;
 import de.neuefische.paulkreft.backend.users.services.UserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mockito;
 
 import java.time.Instant;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 class SignUpServiceTest {
@@ -48,14 +49,15 @@ class SignUpServiceTest {
     @Test
     void signUpWithEmailTest_whenEmailAlreadyRegistered_ReturnNull() {
         // Given
-        SignUpRequest signUpRequest = new SignUpRequest("example@domain.com", "E#4&W%5X!8eyK@hnGu%p7MMznTF6Q#iT%7LVi4ZpaQ#Sqwr4Ud6ZJV7Kj#KHyiQbakCJt$PmkuLAqK!%9T8bn%qtk$hP6t^6DGvt6");
+        SignUpRequest signUpRequest = new SignUpRequest("example@domain.com", "V!Qzx^sgSNe9XnJ8wTkem");
         when(userService.existsByEmail("example@domain.com")).thenReturn(true);
 
         // When
-        UserGet actual = signUpService.signUpWithEmail(signUpRequest);
+        Executable executable =
+                () -> signUpService.signUpWithEmail(signUpRequest);
 
         // Then
-        assertNull(actual);
+        assertThrows(EmailAlreadyRegisteredException.class, executable);
     }
 
     static SignUpRequest[] invalidSignUpRequests() {
@@ -81,7 +83,12 @@ class SignUpServiceTest {
 
     @ParameterizedTest
     @MethodSource("invalidSignUpRequests")
-    void signUpWithEmailTest_invalidSignUps(SignUpRequest signUpRequest) {
-        assertNull(signUpService.signUpWithEmail(signUpRequest));
+    void signUpWithEmailTest_whenInvalidSignUps_thenThrowIllegalArgumentException(SignUpRequest signUpRequest) {
+        // When
+        Executable executable =
+                () -> signUpService.signUpWithEmail(signUpRequest);
+
+        // Then
+        assertThrows(IllegalArgumentException.class, executable);
     }
 }
