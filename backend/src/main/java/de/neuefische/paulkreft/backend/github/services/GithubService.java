2d289b0e19ea77f10c1a4589763bd082406dacd4
paulkreft <paulykreft@gmail.com>
2024-02-07 17:22:30 +0100

    write tests for github service

--- backend/src/main/java/de/neuefische/paulkreft/backend/github/services/GithubService.java
+++ backend/src/main/java/de/neuefische/paulkreft/backend/github/services/GithubService.java
@@ -3,7 +3,7 @@ package de.neuefische.paulkreft.backend.github.services;
 import de.neuefische.paulkreft.backend.exception.GithubEmailNotFoundException;
 import de.neuefische.paulkreft.backend.github.models.GithubEmailResponse;
 import jakarta.servlet.http.HttpServletRequest;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
@@ -14,25 +14,25 @@ import org.springframework.web.client.RestClient;
 import java.util.Optional;
 
 @Service
-@RequiredArgsConstructor
 public class GithubService {
     private final OAuth2AuthorizedClientRepository authorizedClientRepo;
+    private final RestClient restClient;
+
+    public GithubService(OAuth2AuthorizedClientRepository authorizedClientRepo, @Value("https://api.github.com/user/emails") String url) {
+        this.authorizedClientRepo = authorizedClientRepo;
+        this.restClient = RestClient.builder().baseUrl(url).build();
+    }
 
     public String getUserEmail(HttpServletRequest request, OAuth2AuthenticationToken auth) {
         final var authorizedClient = authorizedClientRepo.loadAuthorizedClient(auth.getAuthorizedClientRegistrationId(), auth, request);
         String token = Optional.ofNullable(authorizedClient).map(OAuth2AuthorizedClient::getAccessToken).map(OAuth2AccessToken::getTokenValue).orElse(null);
 
-
-        RestClient restClient = RestClient.builder()
-                .baseUrl("https://api.github.com/user/emails")
-                .defaultHeader("Authorization", "Bearer " + token)
-                .build();
-
         GithubEmailResponse[] response = restClient.get()
+                .header("Authorization", "Bearer " + token)
                 .retrieve()
                 .body(GithubEmailResponse[].class);
 
-        if(response == null || response.length == 0) {
+        if (response == null || response.length == 0) {
             throw new GithubEmailNotFoundException("Could not retrieve GitHub email information!");
         }
 
