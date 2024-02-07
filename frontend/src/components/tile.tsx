06e36c627eda7e262872acfbcc19e6c3834ce3e4
paulkreft <paulykreft@gmail.com>
2024-02-07 13:19:30 +0100

    fix sonarcloud

--- frontend/src/components/tile.tsx
+++ frontend/src/components/tile.tsx
@@ -12,17 +12,17 @@ type TileProps = {
 
 export const Tile: React.FC<TileProps> = ({ color, isClickable, isSelected, isWrong, isCorrect, onClick }) => {
   return (
-    <div
+    <button
       className={cn(
         "h-20 w-20 rounded-xl text-blue-600 sm:h-28 sm:w-28",
         isSelected ? "border-2 border-white outline outline-2 outline-black" : "",
         isWrong ? "border-4 border-white outline outline-4 outline-[#BA2D0B]" : "",
         isCorrect ? "border-4 border-white outline outline-4 outline-[#73BA9B]" : "",
-        isClickable ? "cursor-pointer" : "cursor-default",
         ["#fff", "#FFFFFF", "#ffffff", "#FFF"].includes(color) ? "border border-black" : "",
       )}
-      onClick={isClickable ? onClick : () => {}}
+      onClick={ onClick }
       style={{ backgroundColor: color }}
+      disabled={!isClickable}
     />
   );
 };
