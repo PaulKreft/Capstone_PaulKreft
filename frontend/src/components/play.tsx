9210abed8735266f7f84870ff3bd7e4388132fcb
paulkreft <paulykreft@gmail.com>
2024-02-07 12:31:18 +0100

    refactor tile config to own component; add game over event; save game

--- frontend/src/components/play.tsx
+++ frontend/src/components/play.tsx
@@ -1,10 +1,11 @@
-import { useEffect, useState } from "react";
-import { addHexColors, getRandomHexColor, subtractHexColors } from "../lib/hexUtils.ts";
-import { shuffleArray } from "../lib/shuffleArray.tsx";
+import React, { useEffect, useState } from "react";
 import { cn } from "../lib/utils.ts";
 import { StopWatch } from "./stop-watch.tsx";
-import { Button } from "./Button.tsx";
-import { Tile } from "./Tile.tsx";
+import { Button } from "./button.tsx";
+import { TileConfiguration } from "./tile-configuration.tsx";
+import { addHexColors, getRandomHexColor, subtractHexColors } from "../lib/hexUtils.ts";
+import { shuffleArray } from "../lib/shuffleArray.tsx";
+import axios from "axios";
 
 const WHITE = "#ffffff";
 const BLACK = "#000000";
@@ -15,190 +16,93 @@ const HARD = 4;
 
 type Difficulty = 1 | 2 | 4;
 
-type Tile = {
-  id: string;
-  color: string;
+type PlayProps = {
+  userId?: string;
 };
 
-export default function Play() {
-  const [hasStarted, setHasStarted] = useState<boolean>(false);
-  const [hasLost, setHasLost] = useState<boolean>(false);
-  const [isOver, setIsOver] = useState<boolean>(false);
+export const Play: React.FC<PlayProps> = ({ userId }) => {
+  const [colorConfig, setColorConfig] = useState<string[]>([]);
 
   const [difficulty, setDifficulty] = useState<Difficulty>(EASY);
 
-  const [lastMatch, setLastMatch] = useState<string>("");
-
-  const [sourceTile, setSourceTile] = useState<string>("");
-  const [targetTile, setTargetTile] = useState<string>("");
-  const [currentConfig, setCurrentConfig] = useState<Tile[]>([]);
-  const [nextConfig, setNextConfig] = useState<Tile[]>([]);
+  const [streak, setStreak] = useState<number>(0);
 
   const [startTime, setStartTime] = useState<number>();
   const [endTime, setEndTime] = useState<number>();
 
-  const [streak, setStreak] = useState<number>(0);
-
   useEffect(() => {
-    const additionalTilePairs: Tile[] = [
-      { id: "1-classic", color: WHITE },
-      { id: "2-classic", color: BLACK },
-      { id: "3-classic", color: WHITE },
-      { id: "4-classic", color: BLACK },
-    ];
-    for (let i = 5; i < difficulty * 2 + 5; i += 2) {
-      additionalTilePairs.push({ id: `${i}-classic`, color: WHITE }, { id: `${i + 1}-classic`, color: BLACK });
-    }
-
-    setCurrentConfig(JSON.parse(JSON.stringify(additionalTilePairs)));
-    setNextConfig(JSON.parse(JSON.stringify(additionalTilePairs)));
-    resetGameState();
+    setColorConfig(
+      Array(4 + difficulty * 2)
+        .fill("")
+        .map((_value, index) => (index % 2 === 0 ? BLACK : WHITE)),
+    );
   }, [difficulty]);
 
-  const resetGameState = (): void => {
-    setSourceTile("");
-    setTargetTile("");
-    setLastMatch("");
-    setIsOver(false);
-    setHasLost(false);
-  };
+  const handleOverEvent = (result: "lost" | "won"): void => {
+    const now: number = Date.now();
+    const hasWon: boolean = result === "won";
 
-  useEffect(() => {
-    const blackTiles: number = nextConfig.filter((tile) => tile.color === BLACK).length;
-    const whiteTiles: number = nextConfig.filter((tile) => tile.color === WHITE).length;
-    const lost: boolean = blackTiles !== whiteTiles;
-
-    if (lost) {
-      setEndTime(Date.now());
-      setHasLost(true);
-      setIsOver(true);
-      setStreak(0);
-      return;
-    }
-
-    setCurrentConfig(JSON.parse(JSON.stringify(nextConfig)));
-    setSourceTile("");
-
-    const isOver = hasStarted && !nextConfig.filter((tile) => tile.color !== WHITE && tile.color !== BLACK).length;
-    if (isOver) {
-      setEndTime(Date.now());
-      setIsOver(true);
+    if (hasWon) {
+      setEndTime(now);
       setStreak((s) => s + 1);
-    }
-  }, [hasStarted, nextConfig]);
-
-  const selectColor = (id: string): void => {
-    if (isOver || !hasStarted) {
-      return;
+    } else {
+      setStreak(0);
     }
 
-    if (!sourceTile) {
-      getLastMatch(id);
-      setSourceTile(id);
-      return;
-    }
-
-    if (sourceTile === id) {
-      setSourceTile("");
-      return;
-    }
-
-    setTargetTile(id);
-    combineTiles(sourceTile, id);
+    axios
+      .post("api/games", {
+        userId,
+        type: "",
+        difficulty,
+        isSuccess: hasWon,
+        duration: hasWon && startTime ? now - startTime : null,
+        configuration: colorConfig,
+      })
+      .then((response) => console.log(response));
   };
 
-  const getLastMatch = (last: string): void => {
-    const lastColor = nextConfig.find((tile) => tile.id === last);
-
-    if (!lastColor) {
-      return;
-    }
-    const match: Tile | undefined = nextConfig.find((tile) => addHexColors(tile.color, lastColor.color) === WHITE);
-    setLastMatch(match ? match.id : "");
+  const resetClock = (): void => {
+    setStartTime(undefined);
+    setEndTime(undefined);
   };
 
-  const combineTiles = (givingId: string, receivingId: string): void => {
-    const givingTile = nextConfig.find((tile) => tile.id === givingId);
-    const receivingTile = nextConfig.find((tile) => tile.id === receivingId);
-
-    if (!givingTile || !receivingTile) {
-      return;
-    }
-
-    receivingTile.color = addHexColors(receivingTile.color, givingTile.color);
-    givingTile.color = subtractHexColors(givingTile.color, givingTile.color);
-
-    setNextConfig(
-      currentConfig.map((tile) => {
-        if (tile.id === givingId) {
-          return givingTile;
-        }
-        if (tile.id === receivingId) {
-          return receivingTile;
-        }
-        return tile;
-      }),
-    );
-  };
-
-  const resetTiles = (): Tile[] => {
-    return currentConfig.map((tile, index) => (index % 2 == 0 ? { ...tile, color: BLACK } : { ...tile, color: WHITE }));
+  const resetConfig = (): string[] => {
+    return colorConfig.map((_tile, index) => (index % 2 === 0 ? BLACK : WHITE));
   };
 
   const shuffleColours = (): void => {
-    resetGameState();
-    const baseConfig: Tile[] = resetTiles();
+    resetClock();
+    const baseConfig: string[] = resetConfig();
 
-    const newTiles: Tile[] = [];
-    const whiteTiles = baseConfig.filter((tile) => tile.color === WHITE);
-    const blackTiles = baseConfig.filter((tile) => tile.color === BLACK);
+    const newConfig: string[] = [];
+    const whiteTiles = baseConfig.filter((color) => color === WHITE);
+    const blackTiles = baseConfig.filter((color) => color === BLACK);
 
-    whiteTiles.forEach((tile, index) => {
-      const randomColor = getRandomHexColor(tile.color);
-      newTiles.push({ id: tile.id, color: subtractHexColors(tile.color, randomColor) });
-      newTiles.push({ id: blackTiles[index].id, color: addHexColors(blackTiles[index].color, randomColor) });
+    whiteTiles.forEach((tileColor, index) => {
+      const randomColor = getRandomHexColor(tileColor);
+      newConfig.push(subtractHexColors(tileColor, randomColor));
+      newConfig.push(addHexColors(blackTiles[index], randomColor));
     });
 
-    const shuffledTiles = shuffleArray<Tile>(newTiles);
-
-    setCurrentConfig(shuffledTiles);
-    setNextConfig(shuffledTiles);
-    setHasStarted(true);
+    setColorConfig(shuffleArray<string>(newConfig));
     setStartTime(Date.now());
   };
 
   return (
     <div className="flex flex-1 flex-col items-center justify-center px-5 pb-32 pt-20 xs:pb-20 sm:px-10">
-      <div
-        className={cn(
-          "relative flex flex-wrap justify-center gap-4",
-          currentConfig.length === 6 ? "max-w-80 sm:max-w-96" : "max-w-[24rem] sm:max-w-[32rem]",
-        )}
-      >
-        <div className={cn("absolute -top-10 left-4 text-xl", hasLost ? "text-[#BA2D0B]" : "text-black")}>
+      <TileConfiguration baseConfig={colorConfig} overEvent={handleOverEvent}>
+        <div className={cn("absolute -top-10 left-4 text-xl text-black", streak ? "block" : "hidden")}>
           Streak: {streak}
         </div>
         <StopWatch
-          className={cn("absolute -top-10 right-3 text-xl text-black", isOver && !hasLost ? "block" : "hidden")}
-          value={endTime && startTime ? endTime - startTime : 5999999}
+          className={cn("absolute -top-10 right-3 text-xl text-black", startTime && endTime ? "block" : "hidden")}
+          value={startTime && endTime ? endTime - startTime : null}
         />
-        {currentConfig.map((tile) => (
-          <Tile
-            key={tile.id}
-            onClick={() => selectColor(tile.id)}
-            color={tile.color}
-            isSelected={sourceTile === tile.id}
-            isWrong={targetTile === tile.id && hasLost}
-            isCorrect={lastMatch === tile.id && hasLost}
-            isClickable={isOver || !hasStarted}
-          />
-        ))}
-      </div>
+      </TileConfiguration>
 
       <button className="mt-10 rounded-[20px] border-2 border-black px-12 py-4 text-2xl" onClick={shuffleColours}>
         New Puzzle
       </button>
-
       <div className="mt-10 flex gap-3 xs:gap-5">
         <Button color="#73BA9B" onClick={() => setDifficulty(EASY)} isActive={difficulty === 1}>
           Easy
@@ -212,4 +116,4 @@ export default function Play() {
       </div>
     </div>
   );
-}
+};
---
+++ frontend/src/components/tile-configuration.tsx
@@ -0,0 +1,131 @@
+import React, { useEffect, useState } from "react";
+import { cn } from "../lib/utils.ts";
+
+import { Tile } from "./tile.tsx";
+import { addHexColors } from "../lib/hexUtils.ts";
+
+type TileConfigurationProps = {
+  children: React.ReactNode;
+  baseConfig: string[];
+  overEvent: (result: "lost" | "won") => void;
+};
+
+type Tile = {
+  id: string;
+  color: string;
+};
+
+const WHITE = "#ffffff";
+const BLACK = "#000000";
+
+export const TileConfiguration: React.FC<TileConfigurationProps> = ({ children, baseConfig, overEvent }) => {
+  const [currentConfig, setCurrentConfig] = useState<Tile[]>([]);
+
+  const [isLocked, setIsLocked] = useState<boolean>(false);
+
+  const [actualMatch, setActualMatch] = useState<string>();
+  const [wrongMatch, setWrongMatch] = useState<string>();
+  const [sourceTile, setSourceTile] = useState<Tile>();
+
+  useEffect(() => {
+    setCurrentConfig(baseConfig.map((color, index) => ({ id: `${index}-classic`, color })));
+    resetState();
+  }, [baseConfig]);
+
+  const resetState = (): void => {
+    const isGameInterrupted: boolean = !!currentConfig.filter((tile) => !(tile.color === WHITE || tile.color === BLACK))
+      .length;
+
+    if (isGameInterrupted && !isLocked) {
+      overEvent("lost");
+    }
+
+    setIsLocked(false);
+    setSourceTile(undefined);
+    setActualMatch(undefined);
+    setWrongMatch(undefined);
+  };
+
+  const selectTile = (tile: Tile): void => {
+    if (isLocked || !tile) {
+      return;
+    }
+
+    if (!sourceTile) {
+      setSourceTile(tile);
+      return;
+    }
+
+    if (sourceTile.id === tile.id) {
+      setSourceTile(undefined);
+      return;
+    }
+
+    combineTiles(sourceTile, tile);
+  };
+
+  const getMatchingTile = (tile: Tile): string => {
+    const match: Tile | undefined = currentConfig.find(
+      (matchingTile) => addHexColors(matchingTile.color, tile.color) === WHITE,
+    );
+
+    return match ? match.id : "";
+  };
+
+  const combineTiles = (giving: Tile, receiving: Tile): void => {
+    if (!giving || !receiving) {
+      return;
+    }
+
+    const newColor = addHexColors(receiving.color, giving.color);
+
+    if (newColor !== WHITE) {
+      setActualMatch(getMatchingTile(giving));
+      setWrongMatch(receiving.id);
+      overEvent("lost");
+      setIsLocked(true);
+      return;
+    }
+
+    const newConfig = currentConfig.map((tile) => {
+      if (tile?.id === giving.id) {
+        return { ...tile, color: BLACK };
+      }
+      if (tile?.id === receiving.id) {
+        return { ...tile, color: newColor };
+      }
+      return tile;
+    });
+
+    setSourceTile(undefined);
+
+    if (!newConfig.filter((tile) => !(tile.color === WHITE || tile.color === BLACK)).length) {
+      overEvent("won");
+      setIsLocked(true);
+    }
+
+    setCurrentConfig(newConfig);
+  };
+
+  return (
+    <div
+      className={cn(
+        "relative flex flex-wrap justify-center gap-4",
+        baseConfig.length === 6 ? "max-w-80 sm:max-w-96" : "max-w-[24rem] sm:max-w-[32rem]",
+      )}
+    >
+      {children}
+      {currentConfig.map((tile) => (
+        <Tile
+          key={tile.id}
+          onClick={() => selectTile(tile)}
+          color={tile.color}
+          isSelected={sourceTile?.id === tile.id}
+          isWrong={wrongMatch === tile.id}
+          isCorrect={actualMatch === tile.id}
+          isClickable={!(isLocked || tile.color === WHITE || tile.color === BLACK)}
+        />
+      ))}
+    </div>
+  );
+};
