1b883b7e4de7cc8ddfa1fbc26a48444f4046dd52
paulkreft <paulykreft@gmail.com>
2024-02-12 17:37:41 +0100

    slight style changes

--- frontend/src/components/Profile.tsx
+++ frontend/src/components/Profile.tsx
@@ -44,10 +44,10 @@ export const Profile: React.FC<PlayProps> = ({ user }) => {
     <div className="flex h-max flex-1 flex-col items-center px-5 pb-32 pt-20 xs:pb-20 sm:px-10">
       <div className="w-5/6">
         <div className="flex h-min w-full flex-col gap-2 rounded-2xl border-2 border-black px-6 py-4">
-          <h3 className="mb-2 font-bold">User Information</h3>
-          <ProfileUserInfo className="mb-5" user={user} />
+          <h3 className="mb-2 text-xl">User Information</h3>
+          <ProfileUserInfo className="mb-5 flex flex-col gap-2" user={user} />
 
-          <h3 className="mb-2 font-bold">Statistics</h3>
+          <h3 className="mb-2 text-xl">Statistics</h3>
 
           {statistics.map((statistic) => (
             <Statistic
