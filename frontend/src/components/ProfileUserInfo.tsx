a61d0e36d815d3921a67c9807a49d8ef5250cd02
paulkreft <paulykreft@gmail.com>
2024-02-12 17:38:15 +0100

    make username editable

--- frontend/src/components/ProfileUserInfo.tsx
+++ frontend/src/components/ProfileUserInfo.tsx
@@ -1,5 +1,6 @@
 import React, { ChangeEvent, useState } from "react";
 import { User } from "../types/User.ts";
+import axios from "axios";
 
 type ProfileUserInfoProps = {
   className: string;
@@ -7,28 +8,50 @@ type ProfileUserInfoProps = {
 };
 
 export const ProfileUserInfo: React.FC<ProfileUserInfoProps> = ({ className, user }) => {
-
-  // TODO add default random name, like "Erratic Bunny #1" to a newly created user
   const [name, setName] = useState<string>(user?.name ?? "No username found");
   const [isEditingName, setIsEditingName] = useState<boolean>(false);
+
+  if (!user) {
+    return <></>;
+  }
 
   const onNameChange = (event: ChangeEvent<HTMLInputElement>): void => {
     setName(event.target.value);
   };
 
+  const saveName = (): void => {
+    axios.put("api/user", { ...user, name }).then(() => setIsEditingName(false));
+  };
+
   return (
     <div className={className}>
-      {isEditingName ? (
-        <input
-          className="flex h-max items-center rounded-lg border-2 border-black px-3 py-1 font-light"
-          type="text"
-          value={name}
-          onChange={onNameChange}
-          onBlur={() => setIsEditingName(false)}
-        />
-      ) : (
-        <button onClick={() => setIsEditingName(true)}>{name}</button>
-      )}
+      <div className="flex max-w-96 justify-between items-center">
+        <div className="font-bold">Username</div>
+        {isEditingName ? (
+          <div className="flex gap-5">
+            <input
+              className="h-max rounded-lg border-2 border-black px-3 py-1 font-light"
+              type="text"
+              value={name}
+              onChange={onNameChange}
+            />
+            <button
+              className="h-max items-center rounded-lg border-2 border-black px-3 py-1 font-light hover:bg-black hover:text-white"
+              onClick={saveName}
+            >
+              Save
+            </button>
+          </div>
+        ) : (
+          <button className="cursor-text" onClick={() => setIsEditingName(true)}>
+            {name}
+          </button>
+        )}
+      </div>
+      <div className="flex max-w-96 justify-between">
+        <div className="font-bold">Email</div>
+        <div>{user.email}</div>
+      </div>
     </div>
   );
 };
