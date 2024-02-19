cfb54e21fd3214b95b3f559efa5703f4da36706f
paulkreft <paulykreft@gmail.com>
2024-02-19 16:20:05 +0100

    moved lobby cleanup to backend

--- frontend/src/components/MultiplayerSession.tsx
+++ frontend/src/components/MultiplayerSession.tsx
@@ -27,26 +27,7 @@ export const MultiplayerSession: React.FC<ActiveLobbyProps> = ({ user }) => {
     startListening();
 
     return () => {
-      // on navigating away from the lobby component, leave the lobby
-      axios.put(`/api/lobby/${id}/leave`, player).then((response) => {
-        console.log(response.data);
-
-        // if you were the last one in the lobby, delete the lobby
-        if (!response.data.players.length) {
-          axios.delete(`/api/lobby/${id}`).then((response) => console.log(response));
-          return;
-        }
-
-        // otherwise, if was host, transfer ownership
-        if (response.data.host.id === player.id) {
-          axios
-            .put(`/api/lobby`, {
-              ...response.data,
-              host: response.data.players[0],
-            })
-            .then((response) => console.log(response));
-        }
-      });
+      axios.put(`/api/lobby/${id}/leave`, player).then(() => {});
     };
   }, []);
 
@@ -169,7 +150,9 @@ export const MultiplayerSession: React.FC<ActiveLobbyProps> = ({ user }) => {
           <span className="font-extrabold">{lobby.winner?.name}</span> won!
         </div>
 
-        <div className="mb-16 mt-3 text-2xl font-thin">in <span className="font light">{((lobby.timeToBeat ?? 0) / 1000).toFixed(3)}</span> seconds</div>
+        <div className="mb-16 mt-3 text-2xl font-thin">
+          in <span className="font light">{((lobby.timeToBeat ?? 0) / 1000).toFixed(3)}</span> seconds
+        </div>
         {lobby.host.id === player.id ? (
           <button
             className="h-max items-center rounded-2xl border-2 border-black bg-black px-12 py-4 text-3xl font-light text-white hover:bg-white hover:text-black"
