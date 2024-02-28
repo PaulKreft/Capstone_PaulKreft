1ff950af9e1e22707793eb6e771189d322bdf77c
paulkreft <paulykreft@gmail.com>
2024-02-28 18:26:55 +0100

    refactor fallback away from GlobalExceptionHandler

--- backend/src/main/java/de/neuefische/paulkreft/backend/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/de/neuefische/paulkreft/backend/exception/GlobalExceptionHandler.java
@@ -1,29 +1,14 @@
 package de.neuefische.paulkreft.backend.exception;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-    private final String environment;
-
-    public GlobalExceptionHandler(@Value("${app.environment}") String environment) {
-        this.environment = environment;
-    }
-
-    @ExceptionHandler({NoResourceFoundException.class})
-    public ModelAndView noResourceFoundHandler(NoResourceFoundException e) {
-        String basePath = environment.equals("production") ? "/" : "http://localhost:5173/";
-        String route = environment.equals("production") ? "" : e.getResourcePath();
-        return new ModelAndView("redirect:" + basePath + route);
-    }
 
     @ExceptionHandler({EmailAlreadyRegisteredException.class})
     @ResponseStatus(HttpStatus.CONFLICT)
